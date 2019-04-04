cde0888dcc1cc2d1f3b214d78014beda317fb07a
neopuri0224 <neopuri0224@gmail.com>
2019-04-04 22:59:56 +0900

    [Finish]sample_app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '70e632927ed68283aa158293b6b70aba87cf1dc8d6b0314ec6535776a21c06a992ca6bb688d8917b1e0d376ac74d26287807585fada0bddf41d03a544b720ad4'
